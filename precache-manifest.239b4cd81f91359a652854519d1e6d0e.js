96412f37ee28ba5b6a4b3c598cd76490aebf7068
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-06-03 22:25:55 -0400

    Updates

---
+++ precache-manifest.239b4cd81f91359a652854519d1e6d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec0f277647ce49b5c059253c538b8c0",
+    "url": "/library-react-app/index.html"
+  },
+  {
+    "revision": "2b00ac85a260067b5006",
+    "url": "/library-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9633e5a2928d755fab3b",
+    "url": "/library-react-app/static/js/2.630fe886.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/library-react-app/static/js/2.630fe886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b00ac85a260067b5006",
+    "url": "/library-react-app/static/js/main.d86a520e.chunk.js"
+  },
+  {
+    "revision": "01b2915974e82195f567",
+    "url": "/library-react-app/static/js/runtime-main.aebe776b.js"
+  }
+]);
